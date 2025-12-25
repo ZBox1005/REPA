c466b233ab9bc5154fdb2f37a775188eceb87be2
KLi43 <knavejack43@gmail.com>
2025-12-25 13:39:37 -0600

    update training results

--- evaluations/evaluate_imagenet100.sh
+++ evaluations/evaluate_imagenet100.sh
@@ -15,3 +15,16 @@ python evaluations/evaluator.py \
 
 echo ""
 echo "=== Evaluation Complete ==="
+
+
+# Inception Score: 18.110876083374023
+# FID: 89.82761825760417
+# sFID: 21.352788509765105
+# Precision: 0.2226
+# Recall: 0.6141
+
+# Inception Score: 1.9074647426605225
+# FID: 255.52646042261287
+# sFID: 127.39290336197519
+# Precision: 0.28736
+# Recall: 0.681
